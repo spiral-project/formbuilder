51cf91456844b300a95e27ce6d2da2b1409d3351
Alexis Métaireau <alexis@notmyidea.org>
2014-10-11 14:00:41 +0200

    Only display the fields with a renderer

--- src/js/components/FieldList.js
+++ src/js/components/FieldList.js
@@ -14,15 +14,17 @@ var FieldList = React.createClass({
     return <ul id="field-list">
       <li className="optionTitle">Elements</li>
       <ul id="elements">
-    {
+      {
         Object.keys(this.props.fields).map(function(name) {
           var field = this.props.fields[name];
-          return (
-            <li key={name} onClick={ function() {
-                this.props.addFormElement(name);
-              }.bind(this)}>
-              <Glyphicon glyph={field.glyphicon || "plus"} />&nbsp;{this.props.fields[name].name}
-            </li>);
+          if (field.renderer) {
+            return (
+              <li key={name} onClick={ function() {
+                  this.props.addFormElement(name);
+                }.bind(this)}>
+                <Glyphicon glyph={field.glyphicon || "plus"} />&nbsp;{this.props.fields[name].name}
+              </li>);
+          }
         }.bind(this))
       }</ul></ul>;
   }
