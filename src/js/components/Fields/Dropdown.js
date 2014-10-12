3ab99712be89cae7738705e86402e4df9bd46239
Alexis Métaireau <alexis@notmyidea.org>
2014-10-12 03:16:17 +0200

    Add Dropdown and Radiobuttons renderers.
    It's fun to see how they can reuse the checkboxes editor.

---
+++ src/js/components/Fields/Dropdown.js
@@ -0,0 +1,32 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+var CheckboxesEditor = require("./Checkboxes").Editor;
+
+var DropdownRenderer = React.createClass({
+  render: function() {
+    var values = this.props.data.values || [];
+    return <form className="form-horizontal" role="form">
+      <div className="form-group">
+        <label htmlFor="label" className="col-sm-4 control-label">
+          {this.props.data.label || "Label"}
+        </label>
+        <div className="col-sm-8">
+        <select className="form-control">
+        { values.map(function(value, i) {
+          return <option key={i}>{value}</option>
+        })}
+        </select>
+        </div>
+      </div>
+    </form>
+  }
+});
+
+module.exports = {
+  Editor: CheckboxesEditor,
+  Renderer: DropdownRenderer
+}
+
---
+++ src/js/components/Fields/RadioButtons.js
@@ -0,0 +1,35 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+var CheckboxesEditor = require("./Checkboxes").Editor;
+
+var RadioButtonsRenderer = React.createClass({
+  render: function() {
+    var values = this.props.data.values || [];
+    return <form className="form-horizontal" role="form">
+      <div className="form-group">
+        <label htmlFor="label" className="col-sm-4 control-label">
+          {this.props.data.label || "Label"}
+        </label>
+        <div className="col-sm-8">
+        { values.map(function(value, i) {
+          return <div key={i} className="radio">
+            <label>
+              <input type="radio" /> {value}
+            </label>
+          </div>
+          })
+        }
+        </div>
+      </div>
+    </form>
+  }
+});
+
+module.exports = {
+  Editor: CheckboxesEditor,
+  Renderer: RadioButtonsRenderer
+}
+
